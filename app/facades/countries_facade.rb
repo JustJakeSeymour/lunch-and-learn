8fd69138459a21532f4180e9d539c68671944d06
J Seymour <JustJakeSeymour@gmail.com>
2023-03-04 22:19:47 -0700

    countries random country

--- app/facades/countries_facade.rb
+++ app/facades/countries_facade.rb
@@ -1,5 +1,12 @@
 class CountriesFacade
-  def self.one_country
+  def self.random_country
     json = CountriesService.all_countries
+    countries_array(json).sample(1).first
+  end
+
+  def self.countries_array(json)
+    json.map do |data|
+      Country.new(data)
+    end
   end
 end
--- spec/facades/countries_facade_spec.rb
+++ spec/facades/countries_facade_spec.rb
@@ -3,8 +3,8 @@ require 'rails_helper'
 RSpec.describe CountriesFacade, type: :facade do
   describe 'class methods' do
     context 'random country' do
-      it 'one_country', :vcr do
-        expect(CountriesFacade.one_country).to be_a(Country)
+      it 'random_country', :vcr do
+        expect(CountriesFacade.random_country).to be_a(Country)
       end
     end
   end
