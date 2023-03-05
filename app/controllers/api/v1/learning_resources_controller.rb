87cc9e34a124c394f998b5adb5ac12d28ddeaf78
J Seymour <JustJakeSeymour@gmail.com>
2023-03-05 13:24:15 -0700

    edge case, random country for no country param

--- app/controllers/api/v1/learning_resources_controller.rb
+++ app/controllers/api/v1/learning_resources_controller.rb
@@ -1,14 +1,22 @@
 class Api::V1::LearningResourcesController < ApplicationController
   def index
-    render json: LearningResourceSerializer.format(VideosFacade.search_videos(params[:country]), PhotosFacade.country_photos(params[:country]), params[:country])
+    render json: LearningResourceSerializer.format(get_country_video, get_country_photos, country)
   end
 
   private
   def get_country_video
-    VideosFacade.search_videos(params[:country])
+    VideosFacade.search_videos(country)
   end
 
   def get_country_photos
-    PhotosFacade.country_photos(params[:country])
+    PhotosFacade.country_photos(country)
+  end
+
+  def country
+    if params[:country]
+      params[:country]
+    else
+      CountriesFacade.random_country.name
+    end
   end
 end
