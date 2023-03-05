9701bdce9b9ed171b3b66d29da43dec3c372457a
J Seymour <JustJakeSeymour@gmail.com>
2023-03-05 12:12:52 -0700

    recipes request

--- spec/requests/api/v1/recipes_request_spec.rb
+++ spec/requests/api/v1/recipes_request_spec.rb
@@ -8,11 +8,24 @@ RSpec.describe 'recipes API endpoint' do
 
         expect(response).to be_successful
 
-        activity = JSON.parse(response.body, symbolize_names: true)
-
-        expect(activity).to be_a(Hash)
-        expect(activity).to have_key(:activity)
-        expect(activity[:activity]).to be_a(String)
+        recipes = JSON.parse(response.body, symbolize_names: true)
+        
+        expect(recipes).to be_a(Hash)
+        expect(recipes).to have_key(:data)
+        expect(recipes[:data]).to be_an(Array)
+        expect(recipes[:data].first).to have_key(:id)
+        expect(recipes[:data].first).to have_key(:type)
+        expect(recipes[:data].first[:type]).to eq('recipe')
+        expect(recipes[:data].first).to have_key(:attributes)
+        expect(recipes[:data].first[:attributes]).to be_a(Hash)
+        expect(recipes[:data].first[:attributes]).to have_key(:title)
+        expect(recipes[:data].first[:attributes][:title]).to be_a(String)
+        expect(recipes[:data].first[:attributes]).to have_key(:url)
+        expect(recipes[:data].first[:attributes][:url]).to be_a(String)
+        expect(recipes[:data].first[:attributes]).to have_key(:country)
+        expect(recipes[:data].first[:attributes][:country]).to be_a(String)
+        expect(recipes[:data].first[:attributes]).to have_key(:image)
+        expect(recipes[:data].first[:attributes][:image]).to be_a(String)
       end
     end
   end
