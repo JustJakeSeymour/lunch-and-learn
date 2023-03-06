22a8b52fe84afe16de2e6047a5f1a3249ba0492b
J Seymour <JustJakeSeymour@gmail.com>
2023-03-06 09:56:01 -0700

    places service

--- app/services/places_service.rb
+++ app/services/places_service.rb
@@ -1,34 +1,16 @@
-require 'rails_helper'
+class PlacesService
+  def self.tourism_radius(latlong, radius)
+    response = conn.get("places?categories=tourism&filter=circle:#{latlong[1]},#{latlong[0]},#{radius}")
+    parse_json(response)
+  end
 
-RSpec.describe PlacesService, type: :service do
-  describe 'FINAL ASSESSMENT' do
-    context 'class methods' do
-      it 'returns tourism locations in a 2000 meter radius', :vcr do
-        capital_latlng = [-12.05, -77.05]
-        service = PlacesService.tourism_radius(capital_latlng)
-        
-        expect(service).to be_a(Hash)
-        expect(service).to have_key(:type)
-        expect(service[:type]).to eq('featuresCollection')
-        expect(service).to have_key(:features)
-        expect(service[:features]).to be_an(Array)
-
-        expect(service[:features].first).to be_a(Hash)
-        expect(service[:features].first).to have_key(:type)
-        expect(service[:features].first).to have_key(:properties)
-        
-        expect(service[:features].first[:properties]).to be_a(Hash)
-        expect(service[:features].first[:properties]).to have_key(:name)
-        expect(service[:features].first[:properties][:name]).to be_a(String)
-        
-        expect(service[:features].first[:properties]).to have_key(:address_line1)
-        expect(service[:features].first[:properties][:address_line1]).to be_a(String)
-        
-        expect(service[:features].first[:properties]).to have_key(:address_line2)
-        expect(service[:features].first[:properties][:address_line2]).to be_a(String)
-
-        expect(service[:features].first[:properties]).to have_key(:place_id)
-        expect(service[:features].first[:properties][:place_id]).to be_a(String)
-      end
+  def self.conn
+    Faraday.new(url: 'https://api.geoapify.com/v2/') do |f|
+      f.params['apiKey'] = ENV['places_api_key']
     end
   end
+
+  def self.parse_json(response)
+    JSON.parse(response.body, symbolize_names: true)
+  end
+end
